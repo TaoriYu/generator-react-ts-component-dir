f90983c07354bd4724f53b9de99d93008f733762
Olga Klimonova <taoriyu@gmail.com>
2020-06-01 18:25:33 +0300

    feat(index): minor improvements - delete unused chalk import - fix lint errors

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,8 +1,7 @@
-'use strict';
-const Generator = require('yeoman-generator');
-const chalk = require('chalk');
-const yosay = require('yosay');
-const mkdirp = require('mkdirp');
+"use strict";
+const Generator = require("yeoman-generator");
+const yosay = require("yosay");
+const mkdirp = require("mkdirp");
 
 module.exports = class extends Generator {
   constructor(args, opts) {
@@ -17,43 +16,47 @@ module.exports = class extends Generator {
     this._createSassOption();
 
     if (this.options.less === true && this.options.sass === true) {
-      this.log('Wrong flags combination! Choose either less or sass.');
+      this.log("Wrong flags combination! Choose either less or sass.");
     }
   }
 
   async prompting() {
-    this.log(yosay(`Welcome to the react component generator!`));
+    this.log(yosay(`Let's generate react components!`));
   }
 
   writing() {
-    const capitalizeComponentName = this._capitalize(this.options['component_name']);
-    const dir = mkdirp.sync(`${this.options['path']}/${capitalizeComponentName}`);
-    let stylesExt = 'css';
+    const capitalizeComponentName = this._capitalize(
+      this.options.componentName
+    );
+    const dir = mkdirp.sync(`${this.options.path}/${capitalizeComponentName}`);
+    let stylesExt = "css";
 
-    if (this.options['less']) stylesExt = 'less';
-    if (this.options['sass']) stylesExt = 'sass';
+    if (this.options.less) stylesExt = "less";
+    if (this.options.sass) stylesExt = "sass";
 
-    const styleFilename = `${this.options['component_name'].toLowerCase()}.${stylesExt}`;
-
+    const styleFilename = `${this.options.componentName.toLowerCase()}.${stylesExt}`;
 
     this.fs.copyTpl(
-      this.templatePath('Component.ejs'),
+      this.templatePath("Component.ejs"),
       this.destinationPath(dir, `${capitalizeComponentName}.tsx`),
       {
-        component_name: capitalizeComponentName,
-        styles_name: styleFilename,
-        with_styles: this.options['styles']
+        componentName: capitalizeComponentName,
+        stylesName: styleFilename,
+        withStyles: this.options.styles
       }
     );
 
     this.fs.copyTpl(
-      this.templatePath('reexport.ejs'),
-      this.destinationPath(dir, 'index.ts'),
-      { component_name: capitalizeComponentName }
+      this.templatePath("reexport.ejs"),
+      this.destinationPath(dir, "index.ts"),
+      { componentName: capitalizeComponentName }
     );
 
-    if (this.options['styles'] === true) {
-      this.fs.copyTpl(this.templatePath('styles.ejs'), this.destinationPath(dir, styleFilename));
+    if (this.options.styles === true) {
+      this.fs.copyTpl(
+        this.templatePath("styles.ejs"),
+        this.destinationPath(dir, styleFilename)
+      );
     }
   }
 
@@ -62,45 +65,45 @@ module.exports = class extends Generator {
   }
 
   _createComponentNameArgument() {
-    return this.argument('component_name', {
-      desc: 'name of the component',
+    return this.argument("componentName", {
+      desc: "name of the component",
       type: String,
       optional: true,
-      default: 'Component'
+      default: "Component"
     });
   }
 
   _createPathArgument() {
-    return this.argument('path', {
-      desc: 'path where to generate component',
+    return this.argument("path", {
+      desc: "path where to generate component",
       type: String,
       optional: true,
-      default: './'
+      default: "./"
     });
   }
 
   _createStylesOption() {
-    return this.option('styles', {
-      desc: 'styles will be generated if this option is added',
-      alias: 's',
+    return this.option("styles", {
+      desc: "styles will be generated if this option is added",
+      alias: "s",
       type: Boolean,
       default: true
     });
   }
 
   _createLessOption() {
-    return this.option('less', {
-      desc: 'generate less styles files if set',
-      alias: 'le',
+    return this.option("less", {
+      desc: "generate less styles files if set",
+      alias: "le",
       type: Boolean,
       default: false
     });
   }
 
   _createSassOption() {
-    return this.option('sass', {
-      desc: 'generate sass styles files if set',
-      alias: 'sa',
+    return this.option("sass", {
+      desc: "generate sass styles files if set",
+      alias: "sa",
       type: Boolean,
       default: false
     });
